343530cdcd4ce5e8933762b826648d0492fa48af
Alex Chen <y.chen@columbia.edu>
2015-04-30 23:12:04 -0400

    first commit

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,10 +1,21 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function ($scope, FlashCardsFactory) {
+	$scope.flashCards = FlashCardsFactory.getFlashCards();
 
 	$scope.answerQuestion = function (answer, flashCard) {
 		if (!flashCard.answered) {
 			flashCard.answered = true;
 			flashCard.answeredCorrectly = answer.correct;
 		}
+	}
+
+	$scope.categories = [
+	    'MongoDB',
+	    'Express',
+	    'Angular',
+	    'Node'
+	];
+
+	$scope.getCategoryCards = function(category) {
+		
 	}
 });
